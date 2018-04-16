d8d1f125b1362f025cc36d66b68a527c39672391
smithwebtek <brad@smithwebtek.com>
2018-04-16 10:02:14 -0400

    Try scraping Netflix for movie codes, not working with Nokogiri

--- app/models/service.rb
+++ app/models/service.rb
@@ -8,21 +8,13 @@ class Service < ApplicationRecord
 		self.import_genres(genres)
 	end
   
-	# this scrapes a site for about 23,000 genre codes, but they don't work as expected
-	def self.scrape
-		file = 'https://www.finder.com/netflix/genre-list'
-		genres = {}
+	# this scrapes netflix with the given genre 'code', but so far I can't get movie IDs 
+	def self.scrape(code)
+		file = 'http://netflix.com/browse/genre/' + code.to_s + '?so=su'
     page = HTTParty.get(file)
 		parse = Nokogiri::HTML(page)
-		parse.css('div.nfx-genres').css('li').each do |genre|
-			code = genre.css('a').entries[0].values[1].split('/').last.to_i
-			name = genre.children.children.text
-			genres[name] = code
-		end
-		genres.sort_by{:name}
-		self.import_genres(genres)
 	end
-
+  
 	def self.import_genres(genres)
 		genres.each do |k,v|
 			g = Genre.find_or_create_by(title: k, code: v)
